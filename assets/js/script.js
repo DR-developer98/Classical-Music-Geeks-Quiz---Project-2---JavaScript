d1780b885b8dbb9ee73a4907c80068ab8ea0ba61
DR-developer98 <xhfnpf57@students.codeinstitute.net>
2025-03-08 23:11:39 +0100

    Add function that displays Start quiz button when any of the mode buttons are clicked

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,16 @@
 document.addEventListener("DOMContentLoaded", function () {
   //↓START PAGE QUIZ↓
   const allButtons = document.querySelectorAll("button");
-  
+
   /*
   Adds hover effect to all buttons
   */
   for (let btn of allButtons) {
     btn.addEventListener("mouseenter", function () {
-      btn.classList.toggle("buttonSelected");
+      btn.classList.add("buttonHover");
     });
     btn.addEventListener("mouseleave", function () {
-      btn.classList.toggle("buttonSelected");
+      btn.classList.remove("buttonHover");
     });
   }
 
@@ -18,6 +18,7 @@ document.addEventListener("DOMContentLoaded", function () {
   startForm.addEventListener("submit", showModes);
 
   const modeButtons = document.getElementsByClassName("mode");
+  const startQuizButton = document.getElementById("startQuiz");
   /*
    Shows the three mode buttons after clicking on "Choose mode"
    */
@@ -37,11 +38,7 @@ document.addEventListener("DOMContentLoaded", function () {
   /*
   Shows the "Start quiz" button when clicking on any of the modes
   */
-
   function showStartQuizButton(e) {
-    let clickedButton = e.target;
-    const startQuizButton = document.getElementById("startQuiz");
     startQuizButton.classList.toggle("hide-start");
-    clickedButton.classList.toggle("selectedButton");
   }
 });
