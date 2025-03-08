1ae319418b13ed318b2f142809cb4af0780ee8d0
DR-developer98 <xhfnpf57@students.codeinstitute.net>
2025-03-08 21:09:43 +0100

    Replace CSS button:hover rule by using JavaScript event listeners

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,18 +1,27 @@
 document.addEventListener("DOMContentLoaded", function () {
+  const allButtons = document.querySelectorAll("button");
+  for(let btn of allButtons){
+    btn.addEventListener("mouseenter", function() {
+        btn.classList.toggle("buttonSelected");
+    })
+    btn.addEventListener("mouseleave", function() {
+        btn.classList.toggle("buttonSelected");
+    })
+  }
 
   const startForm = document.getElementById("startForm");
   startForm.addEventListener("submit", showModes);
 
-/*
+  /*
 Shows the three mode buttons after clicking on "Choose mode"
 */
   function showModes(e) {
-    const form=e.target;
+    const form = e.target;
     e.preventDefault();
     const modeButtons = document.getElementsByClassName("mode");
     for (let btn of modeButtons) {
       btn.classList.toggle("hide");
     }
-    chooseMode.style = "color: white; background-color: var(--button-color)";
+    chooseMode.classList.toggle("buttonSelected");
   }
 });
